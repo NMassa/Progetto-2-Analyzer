1fb8e3058e99fcf89f9e13af8fc4124fd1140355
Daniele <lvtdnl@gmail.com>
2017-03-22 15:34:59 +0100

    -Bugfix UnsubACK

--- liv7.c
+++ liv7.c
@@ -926,18 +926,8 @@ void liv7(u_int len, const u_char *p) {
 
 				break;
 		}
-/*
-		for (i = 1; i <= len; i++) {
-			if (isprint(*p))myprintf("%c", *p);
-			else myprintf(".");
-			if (isascii(*p))fprintf(mem, "%c", *p);
-			else fprintf(mem, ".");
-			p++;
-			if ((i % 70) == 0)myprintf("\n     |");
-		}*/
 		myprintf("\n");
 		fflush(mem);
 		//decoded = 1;
-
 	}
 }
